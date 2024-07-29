9bfb75d46d20587ce13fe960be5c1585c43d4668
Wai Phone Aung <waiphone83@gmail.com>
2024-07-29 18:53:58 +0630

    Initial commit

---
+++ src/main/java/com/g3/elis/DatGrowthHub3Application.java
@@ -0,0 +1,13 @@
+package com.g3.elis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatGrowthHub3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatGrowthHub3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/g3/elis/DatGrowthHub3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g3.elis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatGrowthHub3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
