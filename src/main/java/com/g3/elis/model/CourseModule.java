762f1309964c4b4549a4ddb13e804cf9e24c4825
KashouMMT <mmyaatthum925@gmail.com>
2024-08-05 16:39:35 +0630

    Added getter and setters to course module

--- src/main/java/com/g3/elis/model/CourseModule.java
+++ src/main/java/com/g3/elis/model/CourseModule.java
@@ -54,5 +54,113 @@ public class CourseModule {
 	
 	@OneToOne(mappedBy = "course_modules")
 	private ModuleGrade moduleGrade;
-	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getModuleTitle() {
+		return moduleTitle;
+	}
+
+	public void setModuleTitle(String moduleTitle) {
+		this.moduleTitle = moduleTitle;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+
+	public int getMaxScore() {
+		return maxScore;
+	}
+
+	public void setMaxScore(int maxScore) {
+		this.maxScore = maxScore;
+	}
+
+	public int getTotalResultScore() {
+		return totalResultScore;
+	}
+
+	public void setTotalResultScore(int totalResultScore) {
+		this.totalResultScore = totalResultScore;
+	}
+
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Timestamp getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Timestamp updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public List<CourseMaterial> getCourseMaterials() {
+		return courseMaterials;
+	}
+
+	public void setCourseMaterials(List<CourseMaterial> courseMaterials) {
+		this.courseMaterials = courseMaterials;
+	}
+
+	public List<CourseAssignment> getCourseAssignment() {
+		return courseAssignment;
+	}
+
+	public void setCourseAssignment(List<CourseAssignment> courseAssignment) {
+		this.courseAssignment = courseAssignment;
+	}
+
+	public Course getCourses() {
+		return courses;
+	}
+
+	public void setCourses(Course courses) {
+		this.courses = courses;
+	}
+
+	public ModuleGrade getModuleGrade() {
+		return moduleGrade;
+	}
+
+	public void setModuleGrade(ModuleGrade moduleGrade) {
+		this.moduleGrade = moduleGrade;
+	}
+
+	public CourseModule(int id, String moduleTitle, String duration, int maxScore, int totalResultScore,
+			Timestamp createdAt, Timestamp updatedAt, List<CourseMaterial> courseMaterials,
+			List<CourseAssignment> courseAssignment, Course courses, ModuleGrade moduleGrade) {
+		super();
+		this.id = id;
+		this.moduleTitle = moduleTitle;
+		this.duration = duration;
+		this.maxScore = maxScore;
+		this.totalResultScore = totalResultScore;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		this.courseMaterials = courseMaterials;
+		this.courseAssignment = courseAssignment;
+		this.courses = courses;
+		this.moduleGrade = moduleGrade;
+	}
+	public CourseModule()
+	{
+		
+	}
 }
