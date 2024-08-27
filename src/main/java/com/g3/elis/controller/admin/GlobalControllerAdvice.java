3453cb588517e820dcec300b0be17ef12327868d
Kyaw Swar <kswar0566@gmail.com>
2024-08-27 16:18:23 +0630

    updating global controller

--- src/main/java/com/g3/elis/controller/admin/GlobalControllerAdvice.java
+++ src/main/java/com/g3/elis/controller/admin/GlobalControllerAdvice.java
@@ -1,5 +1,8 @@
 package com.g3.elis.controller.admin;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
@@ -8,11 +11,21 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.g3.elis.model.User;
 import com.g3.elis.security.LoginUserDetail;
+import com.g3.elis.service.CourseService;
+import com.g3.elis.service.EnrolledCourseService;
 
 
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
+	
+	@Autowired
+	CourseService courseService;
+
+	@Autowired
+	EnrolledCourseService enrollerCourseService;
+
+
 	
     @ModelAttribute
     public void addUserToModel(Model model) {
@@ -24,6 +37,17 @@ public class GlobalControllerAdvice {
                 LoginUserDetail userDetails = (LoginUserDetail) principal;
                 User user = userDetails.getUser();
                 int userId = user.getId();
+                
+                //for course count in instructor role
+                List<User> userone= enrollerCourseService.getEnrolledStudentsByCourseCreatedByInstructorId(userId);
+        		int enrolledStudentNumber = userone.size();
+
+        		int totalCount = courseService.getTotalCourseCourseByUser(userId);
+        		int activatedCount = courseService.getTotalActivatedCourseCountByUser(userId);
+        		model.addAttribute("enrolledStudentNumber", enrolledStudentNumber);
+        		model.addAttribute("totalCount", totalCount);
+        		model.addAttribute("activatedCount", activatedCount);
+
                 model.addAttribute("user", user);
                 model.addAttribute("user_id", userId);
             }
